e1edcbc0d474fcb2ce0a2c1ca1d8f5fbc122290e
Sofia <sofiaruivalz@gmail.com>
2018-12-19 17:55:48 -0300

    mas cambios al Css

--- ofreceme/resources/views/misOfertas.blade.php
+++ ofreceme/resources/views/misOfertas.blade.php
@@ -10,25 +10,26 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="/css/font-awesome.min.css">
-		<link rel="stylesheet" href="/css/stylesProducto.css">
+		<link rel="stylesheet" href="/css/misOfertas.css">
 		<title>Responsive Web Design</title>
 	</head>
 	<body>
 
-	<div class="container" style="overflow:hidden">
+	<div class="container">
 
+		<div class="tituloOfertasDe">
     <!-- Titulo-->
     <h1>Lista de Ofertas de {{Auth::user()->nombre}}</h1>
 		<!-- Lista de ofertas -->
+		</div>
 
-		@foreach (Auth::user()->ofertasRealizadas as $oferta)
+		<article class="articulosViewOfertas">
+			@foreach (Auth::user()->ofertasRealizadas as $oferta)
 
-			<article class="product" style="float:left" >
-				<div class="photo-container" >
-
-					<img class="photo" src="{{$oferta->getPicture()}}" alt="oferta picture">
-
-				</div>
+				<div class="cadaOferta">
+						<div class="photo-container" >
+								<img class="photo" src="{{$oferta->getPicture()}}" alt="oferta picture">
+						</div>
 				<h2>Para el deseo:</h2>
 				<h3><a href="/newOferta/{{$oferta->bringProducto->id}}">{{$oferta->bringProducto->nombre}}</a></h3>
 				<h2>{{$oferta->nombre}}</h2>
@@ -38,7 +39,8 @@
 				<br>
 				<a class="more" href="/OfertaInfo/{{$oferta->id}}">MAS INFO</a>
 				{{-- <a class="btn btn-link" href="/ofertasAestaDemanda">Ver las ofertas que se le hicieron</a> --}}
-			</article>
+				</div>
+		</article>
 
 
 
@@ -82,4 +84,4 @@
 	</body>
 
 
-@endsection --}} 
+@endsection --}}
